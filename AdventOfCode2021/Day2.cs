67c94a3d48aaca555ada9980ad2a7231fecccff5
Alan Matthews <alanmatthews@microsoft.com>
2022-08-26 14:14:08 -0400

    Day 2 Complete

---
+++ AdventOfCode2021/Day2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace AdventOfCode2021
+{
+    public class Day2
+    {
+        private static List<String> lines = File.ReadLines("../../../../day2input.txt").ToList();
+
+        public Day2()
+        {
+        }
+
+        static public int Part1()
+        {
+            int horizontal = 0;
+            int depth = 0;
+
+            foreach (var line in lines)
+            {
+                var sline = line.Split(' ');
+                var command = sline[0];
+                var units = int.Parse(sline[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        horizontal += units;
+                        break;
+
+                    case "up":
+                        depth -= units;
+                        break;
+
+                    case "down":
+                        depth += units;
+                        break;
+                }
+            }
+
+            return horizontal * depth;
+        }
+
+        static public int Part2()
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (var line in lines)
+            {
+                var sline = line.Split(' ');
+                var command = sline[0];
+                var units = int.Parse(sline[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        horizontal += units;
+                        depth += aim * units;
+                        break;
+
+                    case "up":
+                        aim -= units;
+                        break;
+
+                    case "down":
+                        aim += units;
+                        break;
+                }
+            }
+
+            return horizontal * depth;
+        }
+    }
+}
+
--- AdventOfCode2021Tests/AdventOfCode2021Tests.cs
+++ AdventOfCode2021Tests/AdventOfCode2021Tests.cs
@@ -10,4 +10,11 @@ public class AdventOfCode2021Tests
         Assert.AreEqual(Day1.Part1(), 1292);
         Assert.AreEqual(Day1.Part2(), 1262);
     }
+
+    [TestMethod]
+    public void TestDay2()
+    {
+        Assert.AreEqual(Day2.Part1(), 1383564);
+        Assert.AreEqual(Day2.Part2(), 1488311643);
+    }
 }
